26f76e334e2360cfc1b28b20cebaf04c7c45721c
SergeySolod <solodsergey287@gmail.com>
2020-04-04 16:55:44 +0300

    Todo commit with changes

--- src/Todo.js
+++ src/Todo.js
@@ -4,7 +4,7 @@ import {Text, View, StyleSheet} from 'react-native'
 export const Todo = (props) => {
     return (
         <View style={styles.todo}>
-            {props.todo.title}
+            <Text>{props.todo.title}</Text>
         </View>
     )
 }
@@ -16,7 +16,8 @@ const styles = StyleSheet.create({
             padding: 15,
             borderWidth: 1,
             borderColor: '#eee',
-            borderRadius: 5
+            borderRadius: 5,
+            marginBottom: 10
         }
     }
 )

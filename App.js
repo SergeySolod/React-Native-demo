d1a7552964e68f01b6e541c35bacc02bfb58298c
SergeySolod <solodsergey287@gmail.com>
2020-04-04 15:18:45 +0300

    Input

--- App.js
+++ App.js
@@ -5,13 +5,19 @@ import {AddTodo} from './src/AddTodo'
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View>
         <Navbar />
-        <AddTodo />
+        <View style={styles.container}>
+            <AddTodo />
+        </View>
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        paddingHorizontal: 30,
+        paddingVertical: 20
+    }
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -13,10 +13,12 @@ export const AddTodo = () => {
 const styles = StyleSheet.create({
     block: {
         flexDirection: 'row',
-        justifyContent: 'space-between'
+        justifyContent: 'space-between',
+        alignItems: 'center'
     },
     input: {
         width: '70%',
+        padding: 10,
         borderStyle: 'solid',
         borderBottomWidth: 2,
         borderBottomColor: '#3949ab'
